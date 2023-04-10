007d4d49b01f599fcde3f522351fb594b1de5b96
rChrisb <5767@holbertonstudents.com>
2023-04-10 16:07:56 +0200

    Create an endpoint that retrieves the number of each objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,12 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.user import User
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.amenity import Amenity
 
 
 @app_views.route('/status')
@@ -18,10 +24,10 @@ def json_status():
 def number_by_type():
     """count number of object by class"""
     return jsonify({
-        "amenities": f"{storage.count(Amenity)}",
-        "cities": f"{storage.count(City)}",
-        "places": f"{storage.count(Place)}",
-        "reviews": f"{storage.count(Review)}",
-        "states": f"{storage.count(States)}",
-        "users": f"{storage.count(User)}"
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
         })
