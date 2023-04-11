0983a8edccaad88f0f6561866e7bb8d063e80f4c
rChrisb <5767@holbertonstudents.com>
2023-04-11 15:44:33 +0200

    HTTP access control

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,6 +5,7 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from flask import jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
 
@@ -22,6 +23,7 @@ def notfound(app):
         "error": "Not found"
         }), 404
 
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5050, threaded=True)
