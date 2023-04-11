ffb688cf8e2e1360cbdc0a835437497194cdb5de
rChrisb <5767@holbertonstudents.com>
2023-04-11 16:20:12 +0200

    tests for get and count method on dbstorage

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -87,8 +87,10 @@ class TestFileStorage(unittest.TestCase):
     def test_save(self):
         """Test that save properly saves objects to file.json"""
 
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_get(self):
         self.assertEqual(None, models.storage.get(State, 7))
 
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_count(self):
         self.assertEqual(type(models.storage.count()), int)
